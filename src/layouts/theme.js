4651aeda8d033449e3473eab26835a7065fd9cf2
Bao Vu Hoang <baovuhbv@gmail.com>
2020-02-22 15:19:31 +0700

    Custom Scrollbar

--- src/layouts/theme.js
+++ src/layouts/theme.js
@@ -1,19 +1,47 @@
 import { APP_THEME } from '../types/theme';
-import { grey, red } from "@material-ui/core/colors";
+import { grey, blue } from "@material-ui/core/colors";
 
 const appThemeOptions = {
   [APP_THEME.LIGHT]: {
+    overrides: {
+      MuiCssBaseline: {
+        '@global': {
+          'html': {
+            height: '100%',
+            boxSizing: 'border-box',
+            margin: 0,
+            padding: 0,
+          },
+          'body': {
+            height: '100%',
+            margin: 0,
+            padding: 0,
+          },
+          '*::-webkit-scrollbar': {
+            width: '1em'
+          },
+          '*::-webkit-scrollbar-track': {
+            '-webkit-box-shadow': 'inset 0 0 6px rgba(0,0,0,0.1)',
+          },
+          '*::-webkit-scrollbar-thumb': {
+            background: 'linear-gradient(200deg, #2AA7DE, #663399)',
+            outline: '1px solid slategrey',
+            borderRadius: '10px',
+          }
+        }
+      },
+    },
     palette: {
       type: APP_THEME.LIGHT,
       primary: {
-        light: red[200],
+        light: blue[200],
         main: '#fff',
-        dark: red[600]
+        dark: blue[600]
       },
       secondary: {
-        light: red[200],
-        main: red[400],
-        dark: red[600]
+        light: blue[200],
+        main: blue[400],
+        dark: blue[600]
       },
       background: {
         default: '#fff',
@@ -22,6 +50,34 @@ const appThemeOptions = {
     },
   },
   [APP_THEME.DARK]: {
+    overrides: {
+      MuiCssBaseline: {
+        '@global': {
+          'html': {
+            height: '100%',
+            boxSizing: 'border-box',
+            margin: 0,
+            padding: 0,
+          },
+          'body': {
+            height: '100%',
+            margin: 0,
+            padding: 0,
+          },
+          '*::-webkit-scrollbar': {
+            width: '1em'
+          },
+          '*::-webkit-scrollbar-track': {
+            '-webkit-box-shadow': 'inset 0 0 6px rgba(0,0,0,0.1)',
+          },
+          '*::-webkit-scrollbar-thumb': {
+            background: 'linear-gradient(200deg, #2AA7DE, #663399)',
+            outline: '1px solid slategrey',
+            borderRadius: '10px',
+          }
+        }
+      },
+    },
     palette: {
       type: APP_THEME.DARK,
       primary: {
